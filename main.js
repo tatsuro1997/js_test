835576447dc222928429f76960a57eeb8e598566
tatsuro1997 <minagawa@dptr.jp>
2022-01-06 09:54:00 +0900

    階層の深い連想配列

--- main.js
+++ main.js
@@ -15,3 +15,43 @@ const member = {
 };
 
 console.log(member.height);
+
+
+const member_2 = {
+  '本田': {
+    'height': 170,
+    'hobby': 'サッカー'
+  },
+  '香川': {
+    'height': 160,
+    'hobby': 'サッカー'
+  }
+};
+
+console.log(member_2['香川'].height);
+
+
+const member_3 = {
+  '1kumi': {
+    '本田': {
+      'height': 170,
+      'hobby': 'サッカー'
+    },
+    '香川': {
+      'height': 160,
+      'hobby': 'サッカー'
+    }
+  },
+  '2kumi': {
+    '川島': {
+      'height': 170,
+      'hobby': 'サッカー'
+    },
+    '長友': {
+      'height': 155,
+      'hobby': 'サッカー'
+    }
+  }
+}
+
+console.log(member_3['2kumi']['長友'].height);
