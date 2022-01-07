5a9c3075016353c752a4026bf203fc20311e3919
tatsuro1997 <minagawa@dptr.jp>
2022-01-07 10:08:03 +0900

    関数 メソッドチェーン

--- main.js
+++ main.js
@@ -1,57 +1,15 @@
-const array = [1, 2, 3];
+const postal = {
 
-const array_2 = [
-  [1, 2, 3],
-  [1, 2, 3]
-];
+  postalCode: '123-4567',
 
-console.log(array_2[0][2]);
+  checkPstalCode() {
+    const replaced = this.postalCode.replace('-', '').length;
 
-// オブジェクト
-const member = {
-  'name': 'ホンダ',
-  'height': 170,
-  'hobby': 'サッカー'
-};
-
-console.log(member.height);
-
-
-const member_2 = {
-  '本田': {
-    'height': 170,
-    'hobby': 'サッカー'
-  },
-  '香川': {
-    'height': 160,
-    'hobby': 'サッカー'
-  }
-};
-
-console.log(member_2['香川'].height);
-
-
-const member_3 = {
-  '1kumi': {
-    '本田': {
-      'height': 170,
-      'hobby': 'サッカー'
-    },
-    '香川': {
-      'height': 160,
-      'hobby': 'サッカー'
+    if (replaced === 7) {
+      return true;
     }
-  },
-  '2kumi': {
-    '川島': {
-      'height': 170,
-      'hobby': 'サッカー'
-    },
-    '長友': {
-      'height': 155,
-      'hobby': 'サッカー'
-    }
+    return false;
   }
 }
 
-console.log(member_3['2kumi']['長友'].height);
+console.log(postal.checkPstalCode());
